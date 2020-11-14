7034ac75a8431c94be2cb0a52ce8ecdb692b1b19
Maytham mahdi <metham553@gmail.com>
2020-11-14 17:08:46 +0300

    add repo file

---
+++ bestfind-user/src/Repository.js
@@ -0,0 +1,85 @@
+
+
+
+
+export default class Repository {
+
+    
+  fake = [
+
+  
+ {
+    title:"react" , 
+    text:"this is the Test",
+    imgUrl:"src/logo.png",
+    websiteName:" www.example.com",
+    type:"text",
+    category:"programing",
+    review:5,
+    tags:["#jsx" , "#front-end" , "#framework" ]
+
+} ,
+
+ {
+    title:"react" , 
+    text:"this is the Test",
+    imgUrl:"src/logo.png",
+    websiteName:"www.example.com",
+    type:"text",
+    category:"programing",
+    review:5,
+    tags:["#jsx" , "#front-end" , "#framework" ]
+
+}  ,
+
+{
+    title:"react" , 
+    text:"this is the Test",
+    imgUrl:"src/logo.png",
+    websiteName:" www.example.com",
+    type:"text",
+    category:"programing",
+    review:5,
+    tags:["#jsx" , "#front-end" , "#framework" ]
+
+}  ,
+
+{
+    title:"react" , 
+    text:"this is the Test",
+    imgUrl:"src/logo.png",
+    websiteName:" www.example.com",
+    type:"text",
+    category:"programing",
+    review:5,
+    tags:["#jsx" , "#front-end" , "#framework" ]
+
+} ,
+
+{
+    title:"react" , 
+    text:"this is the Test",
+    imgUrl:"src/logo.png",
+    websiteName:" www.example.com",
+    type:"text",
+    category:"programing",
+    review:5,
+    tags:["#jsx" , "#front-end" , "#framework" ]
+
+} ,
+
+{
+    title:"jsx" , 
+    text:"this is the Test",
+    imgUrl:"src/logo.png",
+    websiteName:" www.example.com",
+    type:"text",
+    category:"programing",
+    review:5,
+    tags:["#jsx" , "#front-end" , "#framework" ]
+
+} ,
+
+ ] ;
+
+}
