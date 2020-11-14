690edc6906ff0d8b4a7a2f8549f2c0846aa46722
Maytham mahdi <metham553@gmail.com>
2020-11-14 08:26:27 +0300

    add Home page

---
+++ bestfind-user/src/pages/Home.js
@@ -0,0 +1,15 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+
+
+export default function Home() {
+
+
+
+    return( 
+        <header className="homePage">
+            
+        </header>
+    )
+ 
+}
