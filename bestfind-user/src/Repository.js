ba71fbdf7af568ce4edf30693b677177ff1fb5e0
Maytham mahdi <metham553@gmail.com>
2020-11-25 21:13:41 +0300

    add fake rebo

--- bestfind-user/src/Repository.js
+++ bestfind-user/src/Repository.js
@@ -4,82 +4,89 @@
 
 export default class Repository {
 
+
+        fake(){
+       
+
+            return  [
     
-  fake = [
+      
+                {
+                   title:"Is React.js the Best Javascript Framework ?" , 
+                   text:"React. js¹ is a flexible and efficient front end javascript library for building UI, and is considered the most in demand in the job .",
+                   imgUrl:"src/logo.png",
+                   websiteName:" www.example.com",
+                   type:"text",
+                   category:"programing",
+                   review:5,
+                   tags:["#jsx" , "#front-end" , "#framework" ]
+               
+               } ,
+               
+                {
+                   title:"Is React.js the Best Javascript Framework ?" , 
+                   text:"React. js¹ is a flexible and efficient front end javascript library for building UI, and is considered the most in demand ",
+                   imgUrl:"src/logo.png",
+                   websiteName:"www.example.com",
+                   type:"text",
+                   category:"programing",
+                   review:5,
+                   tags:["#jsx" , "#front-end" , "#framework" ]
+               
+               }  ,
+               
+               {
+                   title:"react" , 
+                   text:"this is the Test",
+                   imgUrl:"src/logo.png",
+                   websiteName:" www.example.com",
+                   type:"text",
+                   category:"programing",
+                   review:5,
+                   tags:["#jsx" , "#front-end" , "#framework" ]
+               
+               }  ,
+               
+               {
+                   title:"react" , 
+                   text:"this is the Test",
+                   imgUrl:"src/logo.png",
+                   websiteName:" www.example.com",
+                   type:"text",
+                   category:"programing",
+                   review:5,
+                   tags:["#jsx" , "#front-end" , "#framework" ]
+               
+               } ,
+               
+               {
+                   title:"react" , 
+                   text:"this is the Test",
+                   imgUrl:"src/logo.png",
+                   websiteName:" www.example.com",
+                   type:"text",
+                   category:"programing",
+                   review:5,
+                   tags:["#jsx" , "#front-end" , "#framework" ]
+               
+               } ,
+               
+               {
+                   title:"jsx" , 
+                   text:"this is the Test",
+                   imgUrl:"src/logo.png",
+                   websiteName:" www.example.com",
+                   type:"text",
+                   category:"programing",
+                   review:5,
+                   tags:["#jsx" , "#front-end" , "#framework" ]
+               
+               } ,
+               
+                ] ;
+                }
 
-  
- {
-    title:"react" , 
-    text:"this is the Test",
-    imgUrl:"src/logo.png",
-    websiteName:" www.example.com",
-    type:"text",
-    category:"programing",
-    review:5,
-    tags:["#jsx" , "#front-end" , "#framework" ]
+    
 
-} ,
-
- {
-    title:"react" , 
-    text:"this is the Test",
-    imgUrl:"src/logo.png",
-    websiteName:"www.example.com",
-    type:"text",
-    category:"programing",
-    review:5,
-    tags:["#jsx" , "#front-end" , "#framework" ]
-
-}  ,
-
-{
-    title:"react" , 
-    text:"this is the Test",
-    imgUrl:"src/logo.png",
-    websiteName:" www.example.com",
-    type:"text",
-    category:"programing",
-    review:5,
-    tags:["#jsx" , "#front-end" , "#framework" ]
-
-}  ,
-
-{
-    title:"react" , 
-    text:"this is the Test",
-    imgUrl:"src/logo.png",
-    websiteName:" www.example.com",
-    type:"text",
-    category:"programing",
-    review:5,
-    tags:["#jsx" , "#front-end" , "#framework" ]
-
-} ,
-
-{
-    title:"react" , 
-    text:"this is the Test",
-    imgUrl:"src/logo.png",
-    websiteName:" www.example.com",
-    type:"text",
-    category:"programing",
-    review:5,
-    tags:["#jsx" , "#front-end" , "#framework" ]
-
-} ,
-
-{
-    title:"jsx" , 
-    text:"this is the Test",
-    imgUrl:"src/logo.png",
-    websiteName:" www.example.com",
-    type:"text",
-    category:"programing",
-    review:5,
-    tags:["#jsx" , "#front-end" , "#framework" ]
-
-} ,
-
- ] ;
 
 }
